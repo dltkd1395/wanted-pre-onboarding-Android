f80c20c3aeda37c731b199d0730eb0b0a557c7a7
dltkd1395 <dltkd1395@github.com>
2022-09-14 14:58:15 +0900

    clean Architecture+MVVM

--- app/src/main/java/com/qure/onboarding/AdatperListener/CategoryAdapterLiistener.kt
+++
@@ -1,8 +0,0 @@
-package com.qure.onboarding.AdatperListener
-
-import com.qure.onboarding.model.Category
-
-interface CategoryAdapterLiistener {
-
-    fun onItemCategory(item: Category)
-}
--- app/src/main/java/com/qure/onboarding/AdatperListener/NewsAdapterListener.kt
+++
@@ -1,9 +0,0 @@
-package com.qure.onboarding.AdatperListener
-
-import com.qure.onboarding.model.Category
-import com.qure.onboarding.model.News
-
-interface NewsAdapterListener {
-
-    fun onItemNews(item: News)
-}
